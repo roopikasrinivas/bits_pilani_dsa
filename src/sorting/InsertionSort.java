6ef9b6132873621c4d0dea1d18608a4a80b3abc2
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2021-09-19 21:30:13 +0530

    Sort and Search

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -14,13 +14,13 @@ public class InsertionSort {
         /* Move elements of arr[0..i-1], that are
         greater than key, to one position ahead
         of their current position */
+            System.out.println("Pass:" +i);
             while (j >= 0 && A[j] > key) {
                 A[j + 1] = A[j];
                 j = j - 1;
+                PrintArray.printArray(A);
             }
             A[j + 1] = key;
-
-            System.out.println("Array after pass " + i);
             PrintArray.printArray(A);
         }
     }
